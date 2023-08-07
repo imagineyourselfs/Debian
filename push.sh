b67f7aeb496395a8e2d2df6f30ea061c4356cf6f
Monkey <om@debian.home>
2023-08-07 18:30:07 +0100

    Monkeys playing with Debian Linux!

--- push.sh
+++ push.sh
@@ -8,8 +8,8 @@ ssh-add ~/.ssh/github_last_debian
 sleep 2
 echo "github ssh-key added"
 git add .
-git commit -m "Monkeys players with Debian is fun!"
+git commit -m "Monkeys playing with Debian Linux!"
 git push git@github.com:imagineyourselfs/Debian
-echo "Looks good to me! Banana Party!"
 sleep 3
 clear 
+echo "Looks good to me! Banana Party!"
